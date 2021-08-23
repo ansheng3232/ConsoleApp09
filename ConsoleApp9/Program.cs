4d429c44e7dd6938c293db9a5934e99e39d1aa65
Chen An <v-chenan1@microsoft.com>
2021-08-23 06:26:47 +0000

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp9
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
